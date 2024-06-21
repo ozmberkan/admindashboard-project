b73739626d3c92bfac3408c18adbf97cee932e2d
ozmberkan <ozmberkan@gmail.com>
2024-06-21 19:16:23 +0300

    commit add

---
+++ src/components/dataTable/DataTable.tsx
@@ -0,0 +1,67 @@
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
+import "./datatable.scss";
+import { Link } from "react-router-dom";
+
+type Props = {
+  columns: GridColDef[];
+  rows: object[];
+  slug: string;
+};
+
+const DataTable = (props: Props) => {
+  const handleDelete = (id: number) => {
+    //delete the item
+    
+    console.log(id + "has been deleted");
+  };
+
+  const actionColumn: GridColDef = {
+    field: "action",
+    headerName: "Action",
+    width: 200,
+    renderCell: (params) => {
+      return (
+        <div className="action">
+          <Link to={`${props.slug}/${params.row.id}`}>
+            <img src="/view.svg" alt="" />
+          </Link>
+          <div className="delete" onClick={() => handleDelete(params.row.id)}>
+            <img src="/delete.svg" alt="" />
+          </div>
+        </div>
+      );
+    },
+  };
+
+  return (
+    <div className="dataTable">
+      <DataGrid
+        rows={props.rows}
+        columns={[...props.columns, actionColumn]}
+        className="dataGrid"
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        slots={{ toolbar: GridToolbar }}
+        slotProps={{
+          toolbar: {
+            showQuickFilter: true,
+            quickFilterProps: { debounceMs: 100 },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+        disableColumnFilter
+        disableDensitySelector
+        disableColumnSelector
+      />
+    </div>
+  );
+};
+
+export default DataTable;
