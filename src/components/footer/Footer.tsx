c0056c8c058c8775f5c353900e56bffed9d29608
ozmberkan <ozmberkan@gmail.com>
2024-06-21 11:41:35 +0300

    deploy

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import "./footer.scss";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <span>Logo</span>
+      <span># Logo Admin Dashboard</span>
+    </div>
+  );
 };
 
 export default Footer;
